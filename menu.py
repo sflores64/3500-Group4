b183b928070676725a0117203873e380e89dae61
indicalien <aedwin102@gmail.com>
2023-12-04 12:57:51 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,60 +1,110 @@
-import pygame 
-import sys
+# Find/Use Mouse Position in Pygame
+import pygame
+import subprocess
 
 pygame.init()
 
-res = (720,720)
+WIDTH = 500
+HEIGHT = 500
+fps = 60
+timer = pygame.time.Clock()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Menu')
+main_menu = False
+font = pygame.font.Font('freesansbold.ttf', 24)
+menu_command = 0
 
-screen = pygame.display.set_mode(res)
 
-color = (255,255,255)
+class Button:
+    def __init__(self, txt, pos):
+        self.text = txt
+        self.pos = pos
+        self.button = pygame.rect.Rect((self.pos[0], self.pos[1]), (260, 40))
 
-color_light = (110,0,0)
+    def draw(self):
+        pygame.draw.rect(screen, 'light gray', self.button, 0, 4)
+        pygame.draw.rect(screen, 'dark gray', [self.pos[0], self.pos[1], 260, 40], 5, 5)
+        text2 = font.render(self.text, True, 'black')
+        screen.blit(text2, (self.pos[0] + 15, self.pos[1] + 7))
 
-color_dark = (0,0,100)
+    def check_clicked(self):
+        if self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+            return True
+        else:
+            return False
 
-width = screen.get_width()
 
-height = screen.get_height()
+def draw_menu():
+    command = -1
+    pygame.draw.rect(screen, 'black', [100, 100, 300, 380])
+    pygame.draw.rect(screen, 'green', [100, 100, 300, 380], 5)
+    pygame.draw.rect(screen, 'white', [120, 120, 260, 40], 0, 5)
+    pygame.draw.rect(screen, 'gray', [120, 120, 260, 40], 5, 5)
+    txt = font.render('Select Option', True, 'black')
+    
+    menu = Button('Select Option', (120, 120))
+    menu.draw()
+    button1 = Button('Play Checkers', (120, 180))
+    button1.draw()
+    button2 = Button('Play Chess', (120, 240))
+    button2.draw()
+    button3 = Button('Test Checkers', (120, 300))
+    button3.draw()
+    button4 = Button('Test Chess', (120, 360))
+    button4.draw()
+    button5 = Button('Exit Menu' , (120, 420))
+    button5.draw()
 
-smallfont = pygame.font.SysFont('Corbel',35)  
-   
-bigfont = pygame.font.SysFont('Corbel',50)
+    if menu.check_clicked():
+        command = 0
+    if button1.check_clicked():
+        command = 1
+    if button2.check_clicked():
+        command = 2
+    if button3.check_clicked():
+        command = 3
+    return command
 
-text1 = bigfont.render('Welcome To The Game', True , color)
 
-text = smallfont.render('quit' , True , color)  
-  
-while True:  
-      
-    for ev in pygame.event.get():  
-          
-        if ev.type == pygame.QUIT:  
-            pygame.quit()  
-              
-          
-        if ev.type == pygame.MOUSEBUTTONDOWN:  
-              
-              
-              
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:  
-                pygame.quit()  
-                  
-      
-    screen.fill((0,150,50))  
-      
-      
-    mouse = pygame.mouse.get_pos()  
-      
-      
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:  
-        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])  
-          
-    else:  
-        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40])  
-      
-    screen.blit(text1 ,(width/4,height/7))
-    screen.blit(text , (width/4,height/2,140,40))  
-      
-     
-    pygame.display.update()  
+def draw_game():
+    menu_btn = Button('Main Menu', (230, 450))
+    menu_btn.draw()
+    menu = menu_btn.check_clicked()
+    return menu
+
+
+def execute(file):
+    try:
+         subprocess.run(['python3', file], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+    
+
+
+run = True
+while run:
+    screen.fill('dark green')
+    timer.tick(fps)
+    if main_menu:
+        menu_command = draw_menu()
+        if menu_command != -1:
+            main_menu = False
+    else:
+        main_menu = draw_game()
+        if menu_command > 0:
+            if menu_command == 1:
+                execute("checkers.py")
+                pygame.QUIT
+                run = False
+            if menu_command == 2:
+                execute("chess.py")
+                pygame.QUIT
+                run = False
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
